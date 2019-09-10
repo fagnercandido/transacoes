2f2dd075cd8380433f7ae511c495b3c82f53f8a3
Fagner Candido <fagner.candido@globalweb.com.br>
2019-09-10 20:42:37 +0100

    added config to test and jpa

--- src/test/java/com/projeto/transacoes/TransacoesApplicationTests.java
+++ src/test/java/com/projeto/transacoes/TransacoesApplicationTests.java
@@ -1,16 +1,28 @@
 package com.projeto.transacoes;
 
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.projeto.servico.TransacaoServico;
+import com.projeto.transacoes.modelo.Transacao;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TransacoesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TransacaoServico transacaoServico;
+    
+    @Test
+    public void testTrackingIdIsCorrect() {
+    	Transacao transacao = transacaoServico.recuperarTransacoes();
+    	assertEquals("dflkajslçkfjdaslkjflçkasdjf54646dsafadsfadsf", transacao.getTrackingId());
+    }
 
 }
