fb13fe6a952e4dba4b2f1cfa2100f0689c6da8fb
Fagner Candido <fagner.candido@globalweb.com.br>
2019-09-17 10:27:58 +0100

    added new functionalities

--- src/main/java/com/projeto/servico/TransacaoServico.java
+++ src/main/java/com/projeto/servico/TransacaoServico.java
@@ -1,53 +1,23 @@
 package com.projeto.servico;
 
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.projeto.transacoes.anotacoes.PagamentoMinimo;
-import com.projeto.transacoes.modelo.Amount;
-import com.projeto.transacoes.modelo.CurrencyCode;
-import com.projeto.transacoes.modelo.Merchant;
-import com.projeto.transacoes.modelo.Pagamento;
-import com.projeto.transacoes.modelo.Terminal;
-import com.projeto.transacoes.modelo.TipoPagamento;
-import com.projeto.transacoes.modelo.TipoTerminal;
 import com.projeto.transacoes.modelo.Transacao;
 import com.projeto.transacoes.persistencia.TransacaoRepository;
 
 @Service
 public class TransacaoServico {
-	
+
 	private TransacaoRepository transacaoRepository;
 
-	public Transacao recuperarTransacoes() {
-		Amount amount = new Amount(BigDecimal.valueOf(120), CurrencyCode.EURO);
-		
-		validador(amount);
-		
-		Merchant merchant = new Merchant(5L, 4L);
-		Terminal terminal =  new Terminal(7L, TipoTerminal.MBWAY);
-		Pagamento pagamento = new Pagamento(TipoPagamento.PAGAMENTO_REFERENCIA);
-		return new Transacao("dflkajslçkfjdaslkjflçkasdjf54646dsafadsfadsf", amount, merchant, terminal, pagamento);
+	public List<Transacao> recuperarTransacoes() {
+		return transacaoRepository.findAll();
 	}
 
-	private boolean validador(Amount amount) {
-		Class<? extends Amount> classe = amount.getClass();
-		for (Field field : classe.getDeclaredFields()) {
-			if (field.isAnnotationPresent(PagamentoMinimo.class)) {
-				PagamentoMinimo pagamentoMinimo = field.getAnnotation(PagamentoMinimo.class);
-				field.setAccessible(true);
-				try {
-					BigDecimal valor = (BigDecimal) field.get(amount);
-					double doubleValue = valor.doubleValue();
-					return doubleValue > pagamentoMinimo.valor();
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return false;
+	public void salvarTransacoes(Transacao transacao) {
+		transacaoRepository.save(transacao);
 	}
 
 }
--- src/main/java/com/projeto/transacoes/rest/TransactionRest.java
+++ src/main/java/com/projeto/transacoes/rest/TransactionRest.java
@@ -1,7 +1,11 @@
 package com.projeto.transacoes.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.servico.TransacaoServico;
@@ -14,10 +18,14 @@ public class TransactionRest {
 	private TransacaoServico transacaoServico;
 	
 	
-	@RequestMapping("/transacoes")
-	//value = "/custom", method = RequestMethod.POST
-    public Transacao recuperarTransacao() {
+	@RequestMapping(value = "/transacoes", method = RequestMethod.GET)
+    public List<Transacao> recuperarTransacoes() {
         return transacaoServico.recuperarTransacoes();
+    }
+	
+	@RequestMapping(value = "/transacoes", method = RequestMethod.POST)
+    public void salvarTransacoes(@RequestBody Transacao transacao) {
+        transacaoServico.salvarTransacoes(transacao);
     }
 
 }
